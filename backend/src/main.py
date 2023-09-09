4bc82e1e9d9c3d0223c2210c1ce42b1acfa54a7b
usersina <benna.anis2@gmail.com>
2023-09-09 13:22:51 +0100

    add sample mariadb database

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,15 @@
+from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
 
+from routes.generate import page as generateRoute
 from routes.index import page as indexRoute
+from routes.querySql import page as querySqlRoute
+
+# Load environment variables
+load_dotenv(
+    dotenv_path=".env",  # Relative to where the script gets executed
+)
 
 app = Flask(__name__)
 # See https://github.com/corydolphin/flask-cors/issues/257
@@ -10,6 +18,8 @@ app.url_map.strict_slashes = False
 CORS(app)
 
 app.register_blueprint(indexRoute, url_prefix="/api")
+app.register_blueprint(generateRoute, url_prefix="/api/generate")
+app.register_blueprint(querySqlRoute, url_prefix="/api/sql")
 
 # Execute only if run as a script
 if __name__ == "__main__":
---
+++ compose/init/README.md
@@ -0,0 +1,7 @@
+# Initialization
+
+This folder contains the initialization scripts for MariaDB (MySQL) and Logstash.
+
+## Resources
+
+- [MySQL Sample Retailer Database](https://www.mysqltutorial.org/mysql-sample-database.aspx)
