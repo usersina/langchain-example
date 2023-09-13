91417ae7cad9796e81a30b1d6c3bc6c3358c901e
usersina <benna.anis2@gmail.com>
2023-09-13 17:55:46 +0100

    add stream route

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,10 +2,11 @@ from dotenv import load_dotenv
 from quart import Quart
 from quart_cors import cors
 
-from routes.chat import page as chatRoute
-from routes.generate import page as generateRoute
-from routes.index import page as indexRoute
-from routes.query_sql import page as querySqlRoute
+from routes.chat import page as chat_route
+from routes.generate import page as generate_oute
+from routes.index import page as index_route
+from routes.query_sql import page as sql_route
+from routes.stream import page as stream_route
 
 # Load environment variables
 load_dotenv(
@@ -18,10 +19,11 @@ app.url_map.strict_slashes = False
 
 cors(app)
 
-app.register_blueprint(indexRoute, url_prefix="/api")
-app.register_blueprint(generateRoute, url_prefix="/api/generate")
-app.register_blueprint(chatRoute, url_prefix="/api/chat")
-app.register_blueprint(querySqlRoute, url_prefix="/api/sql")
+app.register_blueprint(index_route, url_prefix="/api")
+app.register_blueprint(generate_oute, url_prefix="/api/generate")
+app.register_blueprint(chat_route, url_prefix="/api/chat")
+app.register_blueprint(sql_route, url_prefix="/api/sql")
+app.register_blueprint(stream_route, url_prefix="/api/stream")
 
 # Execute only if run as a script
 if __name__ == "__main__":
